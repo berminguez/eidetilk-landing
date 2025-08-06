c1948ad1e056c3a7839a7af64a87722264f01079
bernardominguez <berminguez@gmail.com>
2025-08-06 17:35:00 +0200

    quitamos sponsors

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <SponsorsSection />
+    {/*   <SponsorsSection /> */}
       <BenefitsSection />
       <FeaturesSection />
       <ServicesSection />
